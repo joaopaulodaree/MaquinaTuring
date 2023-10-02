c7b1b50f5aa52c0e826fcab9020beae07a035581
Paulo Henrique Teixeira Pompeu <paulohteixeirap@gmail.com>
2023-10-02 17:41:09 -0300

    pushs

--- src/MaquinaTuring.java
+++ src/MaquinaTuring.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class MaquinaTuring {
@@ -5,10 +6,24 @@ public class MaquinaTuring {
     private int position;
     private String currentState;
     private Transicoes transitions;
+    private ArrayList<String> finalStates = transitions.getFinalStates();
 
     public MaquinaTuring() {
         currentState = transitions.getCurrentState();
     }
 
-    
+    public void verificarPalavra(String palavra) {
+        for (char letra : palavra.toCharArray()) {
+            if (!transitions.transicao(letra)) {
+                System.out.println("A Palavra " + palavra + " não foi aceita!");
+                return;
+            }
+        }
+        if (currentState.equals(finalStates.get(0)))
+            System.out.println("A Palavra " + palavra + " foi aceita!");
+        else
+            System.out.println("A Palavra " + palavra + " não foi aceita!");
+
+    }
+
 }
