a035a0b908986f52fe95b4b979f66be373b9a5bc
Paulo H Pompeu <110269867+PauloHPompeu@users.noreply.github.com>
2023-10-02 17:32:00 -0300

    joinha

--- src/Transicoes.java
+++ src/Transicoes.java
@@ -10,6 +10,24 @@ public class Transicoes {
     public Transicoes(){
         currentState = "q0";
         finalStates.add("qF");
-
+        transitions.put("q0", Map.of('a', "q1", 'b', "q2"));
+        transitions.put("q1", Map.of('a', "qf", 'b', "q2"));
+        transitions.put("q2", Map.of('a', "q1", 'b', "qf"));
+        transitions.put("qf", Map.of('a', "qf", 'b', "qf"));
     }
+    public boolean transicao(char entrada) {
+        if (transicoes.get(currentState).containsKey(entrada)) {
+            currentState = transicoes.get(currentState).get(entrada);
+            return true;
+        } else {
+            currentState = null;
+            return false;
+        }
+    }
+
+    public String getCurrentState(){
+        return currentState;
+    }
+
+
 }
