2dd3e425f2f85204aaee7ef1624f37a3ce1f2275
Joao_Paulo_Souza <joao.silva60@estudante.ufla.br>
2023-10-03 17:34:07 -0300

    Lalaland

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         TuringMachine tm = new TuringMachine();
-        String input = "aababababababab";
+        String input = "10+11";
         tm.processInput(input);
     }
 }
